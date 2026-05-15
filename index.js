82dd77d589f1b48f9ddfb91c556fbe624ccb96bf
nazmul_khan <nazmul.khan121@gmail.com>
2026-05-15 20:40:01 +0600

    connect to the datasbase

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,9 +11,38 @@ app.use(express.json());
 // SimpleCRUD
 // 2mmvo1vT8PSsqNSh
 
+const uri =
+  'mongodb+srv://SimpleCRUD:2mmvo1vT8PSsqNSh@cluster0.wbbieaf.mongodb.net/?appName=Cluster0';
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get('/', (req, res) => {
   res.send('YES!!! Connected');
 });
+
+async function run() {
+  //
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
